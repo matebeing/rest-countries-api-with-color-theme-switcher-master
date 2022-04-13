5a9abeeba519ef0ea8b5e6dff0d724e5e9fc0bf9
Matheus Oliveira <matheusoliveira18rj@gmail.com>
2022-04-13 17:58:30 -0300

    [FIX] Refractured Code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import styled from 'styled-components';
-import './style/App.css'
-
+import React from 'react';
+import Countries from './components/Countries';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <nav>
-          <b>Where in the world?</b>
-          <button>Dark mode</button>
-        </nav>
-      </header>
-      <main>
-        <div className="searchContainer">
-          <input placeholder='Search for a country...' type='text'></input>
-          <select>
-            <option>Filter by Region</option>
-            <option>Africa</option>
-            <option>America</option>
-            <option>Asia</option>
-            <option>Europe</option>
-            <option>Oceania</option>
-          </select>
-        </div>
-        <div className="countriesContainer">
-        </div>
-      </main>
-    </div>
+    <>
+      <Countries />
+    </>
   );
 }
 
--- src/style/homeStyle.js
+++
@@ -1,8 +0,0 @@
-import styled  from "styled-components";
-
-const Heading = styled.h1`
-  font-size: 5rem;
-`;
-
-
-export default Heading;
